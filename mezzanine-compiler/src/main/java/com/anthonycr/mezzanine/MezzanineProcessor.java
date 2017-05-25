cda5f861554f7b0470780d802c2cc45d70cc88a3
anthony restaino <anthonyrestaino11@gmail.com>
2017-05-24 23:05:44 -0400

    Converting rest of subscribe action to mapping functions
    Also left aligning text

--- mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/MezzanineProcessor.java
+++ mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/MezzanineProcessor.java
@@ -5,29 +5,21 @@ import com.anthonycr.mezzanine.generator.MezzanineGenerator;
 import com.anthonycr.mezzanine.map.MezzanineMapper;
 import com.anthonycr.mezzanine.source.ElementSource;
 import com.anthonycr.mezzanine.source.MezzanineElementSource;
-import com.anthonycr.mezzanine.utils.FileGenUtils;
 import com.anthonycr.mezzanine.utils.MessagerUtils;
 import com.google.auto.service.AutoService;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.TypeSpec;
 
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Filer;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 
 @AutoService(Processor.class)
 @SupportedAnnotationTypes(value = {"com.anthonycr.mezzanine.FileStream"})
 public class MezzanineProcessor extends AbstractProcessor {
-
-    private static final String PACKAGE_NAME = "com.anthonycr.mezzanine";
-    private static final String CLASS_NAME = "Mezzanine";
 
     private boolean isProcessed;
 
@@ -55,20 +47,12 @@ public class MezzanineProcessor extends AbstractProcessor {
             .map(MezzanineMapper.elementToTypeAndFilePair())
             .doOnNext(typeElementFileEntry -> MessagerUtils.reportInfo("Processing file: " + typeElementFileEntry.getValue()))
             .map(MezzanineMapper.fileToStringContents())
-            .map(MezzanineGenerator.generateMezzanineTypeSpec())
+            .map(MezzanineGenerator.generateGeneratorTypeSpecs())
             .toList()
-            .subscribe(typeSpecs -> {
-                TypeSpec.Builder mezzanineTypeSpecBuilder = TypeSpec.classBuilder(CLASS_NAME)
-                    .addModifiers(Modifier.PUBLIC);
-
-                typeSpecs.forEach(mezzanineTypeSpecBuilder::addType);
-
-                JavaFile javaFile = JavaFile.builder(PACKAGE_NAME, mezzanineTypeSpecBuilder.build()).build();
-
-                Filer filer = processingEnv.getFiler();
-
-                FileGenUtils.writeToFile(javaFile, filer);
-            });
+            .map(MezzanineGenerator.generateMezzanineTypeSpec())
+            .map(MezzanineGenerator.generateJavaFile())
+            .flatMapCompletable(MezzanineGenerator.writeFileToDisk(processingEnv.getFiler()))
+            .subscribe(() -> MessagerUtils.reportInfo("File successfully processed"));
 
         return true;
     }
--- mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/generator/MezzanineGenerator.java
+++ mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/generator/MezzanineGenerator.java
@@ -1,18 +1,23 @@
 package com.anthonycr.mezzanine.generator;
 
+import com.anthonycr.mezzanine.utils.FileGenUtils;
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
 
+import javax.annotation.processing.Filer;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 
+import io.reactivex.Completable;
 import io.reactivex.functions.Function;
 
 /**
@@ -24,6 +29,9 @@ public class MezzanineGenerator {
 
     private static final String PREFIX = "Generator_";
 
+    private static final String PACKAGE_NAME = "com.anthonycr.mezzanine";
+    private static final String CLASS_NAME = "Mezzanine";
+
     private MezzanineGenerator() {}
 
     /**
@@ -34,7 +42,7 @@ public class MezzanineGenerator {
      * @return a valid mapping function.
      */
     @NotNull
-    public static Function<Map.Entry<TypeElement, String>, TypeSpec> generateMezzanineTypeSpec() {
+    public static Function<Map.Entry<TypeElement, String>, TypeSpec> generateGeneratorTypeSpecs() {
         return typeElementStringEntry -> {
 
             TypeElement typeElement = typeElementStringEntry.getKey();
@@ -55,6 +63,53 @@ public class MezzanineGenerator {
                 .addSuperinterface(ClassName.get(typeElement))
                 .addMethod(methodSpec)
                 .build();
+        };
+    }
+
+    /**
+     * Generates the Mezzanine {@link TypeSpec}
+     * from a list of {@link TypeSpec}.
+     *
+     * @return a valid mapping function.
+     */
+    @NotNull
+    public static Function<List<TypeSpec>, TypeSpec> generateMezzanineTypeSpec() {
+        return typeSpecs -> {
+            TypeSpec.Builder mezzanineTypeSpecBuilder = TypeSpec.classBuilder(CLASS_NAME)
+                .addModifiers(Modifier.PUBLIC);
+
+            typeSpecs.forEach(mezzanineTypeSpecBuilder::addType);
+
+            return mezzanineTypeSpecBuilder.build();
+        };
+    }
+
+    /**
+     * Generates a {@link JavaFile} from
+     * a {@link TypeSpec}, {@link #PACKAGE_NAME}
+     * will be used as the package name.
+     *
+     * @return a valid mapping function.
+     */
+    @NotNull
+    public static Function<TypeSpec, JavaFile> generateJavaFile() {
+        return typeSpec -> JavaFile.builder(PACKAGE_NAME, typeSpec).build();
+    }
+
+    /**
+     * Mapping function that writes a
+     * {@link JavaFile} to disk and emits
+     * a {@link Completable} that completes
+     * when the file has been written to disk.
+     *
+     * @param filer the filer necessary to write to disk.
+     * @return a valid mapping function.
+     */
+    @NotNull
+    public static Function<JavaFile, Completable> writeFileToDisk(@NotNull Filer filer) {
+        return javaFile -> {
+            FileGenUtils.writeToFile(javaFile, filer);
+            return Completable.complete();
         };
     }
 
