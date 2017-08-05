1ed8cfacc024364c3c9f44b793daa040545fb26f
anthony restaino <anthonyrestaino11@gmail.com>
2017-08-05 18:32:11 -0400

    Make constructor private

--- mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/function/GenerateMezzanineTypeSpecFunction.kt
+++ mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/function/GenerateMezzanineTypeSpecFunction.kt
@@ -1,5 +1,6 @@
 package com.anthonycr.mezzanine.function
 
+import com.squareup.javapoet.MethodSpec
 import com.squareup.javapoet.TypeSpec
 import io.reactivex.functions.Function
 import javax.lang.model.element.Modifier
@@ -12,8 +13,13 @@ object GenerateMezzanineTypeSpecFunction : Function<List<TypeSpec>, TypeSpec> {
     private const val CLASS_NAME = "MezzanineGenerator"
 
     override fun apply(typeSpecs: List<TypeSpec>): TypeSpec {
+        val mezzanineConstructorBuilder = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PRIVATE)
+
         val mezzanineTypeSpecBuilder = TypeSpec.classBuilder(CLASS_NAME)
+                .addModifiers(Modifier.FINAL)
                 .addModifiers(Modifier.PUBLIC)
+                .addMethod(mezzanineConstructorBuilder.build())
 
         typeSpecs.forEach { mezzanineTypeSpecBuilder.addType(it) }
 
