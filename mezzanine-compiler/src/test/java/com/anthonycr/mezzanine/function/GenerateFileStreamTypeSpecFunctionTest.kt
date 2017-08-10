c01e70c2ec77d49ab7a715277872e71899c5addd
anthony restaino <anthonyrestaino11@gmail.com>
2017-08-09 21:31:28 -0400

    Tests for GenerateFileStreamTypeSpecFunction

---
+++ mezzanine-compiler/src/test/java/com/anthonycr/mezzanine/function/GenerateFileStreamTypeSpecFunctionTest.kt
@@ -0,0 +1,30 @@
+package com.anthonycr.mezzanine.function
+
+import org.junit.Test
+import org.mockito.Mockito
+import javax.lang.model.element.TypeElement
+import javax.lang.model.element.VariableElement
+
+/**
+ * Additional unit tests for [GenerateFileStreamTypeSpecFunction].
+ */
+class GenerateFileStreamTypeSpecFunctionTest {
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun test_TypeElementWithNoEnclosedElements_Fails() {
+        val typeElement = Mockito.mock(TypeElement::class.java)
+        Mockito.`when`(typeElement.enclosedElements).thenReturn(mutableListOf())
+
+        GenerateFileStreamTypeSpecFunction.apply(Pair(typeElement, ""))
+    }
+
+    @Test(expected = ClassCastException::class)
+    fun test_TypeElementWithNonMethodElement_Fails() {
+        val typeElement = Mockito.mock(TypeElement::class.java)
+        val field = Mockito.mock(VariableElement::class.java)
+        Mockito.`when`(typeElement.enclosedElements).thenReturn(mutableListOf(field))
+
+        GenerateFileStreamTypeSpecFunction.apply(Pair(typeElement, ""))
+    }
+
+}
