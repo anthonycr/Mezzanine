06a82dae33e7c1c28d5389ae92b0123f1042608d
anthony restaino <anthonyrestaino11@gmail.com>
2017-08-09 21:16:26 -0400

    Adding unit tests for ElementToTypeAndFilePairFunction

--- mezzanine-compiler/src/test/java/com/anthonycr/mezzanine/MezzanineProcessorTest.kt
+++ mezzanine-compiler/src/test/java/com/anthonycr/mezzanine/MezzanineProcessorTest.kt
@@ -2,6 +2,7 @@ package com.anthonycr.mezzanine
 
 import org.junit.Test
 import testcase.InvalidFileTestCase
+import testcase.ValidFileExtraSlashTestCase
 import testcase.ValidFileTestCase
 import javax.tools.Diagnostic
 
@@ -20,9 +21,17 @@ class MezzanineProcessorTest : AbstractAnnotationProcessorTest() {
     }
 
     @Test
+    fun testValidFileExtraSlashCase_Succeeds() {
+        val output = compileTestCase(ValidFileExtraSlashTestCase::class.java)
+
+        assertCompilationSuccessful(output)
+    }
+
+    @Test
     fun test_InvalidFileCase_Fails() {
         val output = compileTestCase(InvalidFileTestCase::class.java)
 
         assertCompilationReturned(arrayOf(Diagnostic.Kind.ERROR), longArrayOf(9), output)
     }
+
 }
---
+++ mezzanine-compiler/src/test/java/com/anthonycr/mezzanine/function/ElementToTypeAndFilePairFunctionTest.kt
@@ -0,0 +1,19 @@
+package com.anthonycr.mezzanine.function
+
+import org.junit.Test
+import org.mockito.Mockito
+import javax.lang.model.element.ExecutableElement
+
+/**
+ * Additional unit tests for [ElementToTypeAndFilePairFunction].
+ */
+class ElementToTypeAndFilePairFunctionTest {
+
+    @Test(expected = Exception::class)
+    fun test_NonTypeElement_ThrowsException() {
+        val executableElement = Mockito.mock(ExecutableElement::class.java)
+
+        ElementToTypeAndFilePairFunction.apply(executableElement)
+    }
+
+}
