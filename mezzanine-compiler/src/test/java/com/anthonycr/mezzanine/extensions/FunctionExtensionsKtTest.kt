0557876b256432989ab637b131523d5b54bb2682
anthony restaino <anthonyrestaino11@gmail.com>
2017-08-11 19:34:37 -0400

    Do on next extension test

---
+++ mezzanine-compiler/src/test/java/com/anthonycr/mezzanine/extensions/FunctionExtensionsKtTest.kt
@@ -0,0 +1,23 @@
+package com.anthonycr.mezzanine.extensions
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+/**
+ * Unit tests for extensions.
+ */
+class FunctionExtensionsKtTest {
+
+
+    @Test
+    fun test_DoOnNext() {
+        val listOne = listOf("one", "two", "three")
+
+        val listTwo = mutableListOf<String>()
+
+        val listThree = listOne.doOnNext { listTwo.add(it) }
+
+        Assertions.assertThat(listOne).isEqualTo(listTwo)
+        Assertions.assertThat(listOne).isEqualTo(listThree)
+    }
+}
