11f832e3b7908239602a0bdb6d0abc528d84a787
anthony restaino <anthonyrestaino11@gmail.com>
2017-08-10 23:28:26 -0400

    Making extension functions inline

--- mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/extensions/FunctionExtensions.kt
+++ mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/extensions/FunctionExtensions.kt
@@ -3,14 +3,14 @@ package com.anthonycr.mezzanine.extensions
 /**
  * Generic map function that allows mapping from any type to another type.
  */
-fun <T, R> T.with(predicate: (T) -> R): R {
+inline fun <T, R> T.with(predicate: (T) -> R): R {
     return predicate.invoke(this)
 }
 
 /**
  * Allows for arbitrary execution in the middle of a stream based on a value.
  */
-fun <T> List<T>.doOnNext(predicate: (T) -> Unit): List<T> {
+inline fun <T> List<T>.doOnNext(predicate: (T) -> Unit): List<T> {
     this.forEach { predicate.invoke(it) }
     return this
 }
