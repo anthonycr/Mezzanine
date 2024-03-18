7c419b2a94618ea6e9168800b136330bb9990f3b
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-03-17 20:57:58 -0400

    Convert settings.gradle to kotlin and delete broken tests

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mezzanine"
+include(":sample")
+include(":mezzanine")
+include(":mezzanine-compiler")
