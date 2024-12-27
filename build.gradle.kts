595389d034bfdc63b9f173b3150c0eb302149dbb
Anthony Restaino <anthonyrestaino11@gmail.com>
2024-12-27 16:33:22 -0500

    Add non applied plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.app) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
 }
