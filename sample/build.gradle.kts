269c739bc47f86f2b8bd22bf6c55a01431b4a054
Anthony Restaino <anthonyrestaino11@gmail.com>
2025-10-18 08:25:50 -0400

    target sdk 36

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.anthonycr.mezzanine"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.anthonycr.mezzanine"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
